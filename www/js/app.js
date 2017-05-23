c1d1662634a32d6fcd02dbee9e646ada0da8545d
Soham Banerjee <soham2008xyz@gmail.com>
2017-05-23 14:19:31 +0530

    Add sticky list header support

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('streamy', ['ionic', 'streamy.controllers', 'streamy.services', 'streamy.filters']);
+var app = angular.module('streamy', ['ionic', 'ion-sticky', 'streamy.controllers', 'streamy.services', 'streamy.filters']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function () {
